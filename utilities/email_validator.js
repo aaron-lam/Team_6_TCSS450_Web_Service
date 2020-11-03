4fa3bd3e2cb3988cd499b5e032265fa16a992cb7
Anthony <nguyenanthony80@gmail.com>
2020-11-02 20:40:37 -0800

    added simple email checker

---
+++ utilities/email_validator.js
@@ -0,0 +1,25 @@
+
+/**
+ * Returns a message when email is not the proper format
+ * @param {String} email the email to check
+ */
+function validateEmail(email) {
+
+    splitEmail = email.split('@')
+    if(splitEmail.length > 2) {
+        return 'Email can only contain one @'
+    }
+    local = splitEmail[0]
+    domain = splitEmail[1]
+    illegalChars = ['+', '"', ",", " "]
+    
+    illegalChars.forEach(char => {
+        if(domain.includes(char)) return `${char} cannot be present before @`
+    })
+
+    if(!domain.includes('.')) return 'Domain must contain a period'
+
+}
+
+
+module.exports = validateEmail;
